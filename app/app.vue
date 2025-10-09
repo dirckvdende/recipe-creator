2f535a218aa51def24fc8c69f02ca1270703d404
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-10-09 22:08:17 +0200

    Started learning nuxt

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue"
+
+const count = ref(0);
+
+function increment() {
+    count.value++;;
+}
+</script>
+
+<template>
+    <button @click="increment">{{ count }}</button>
+    <div>
+        <h1>Hello World!</h1>
+    </div>
+</template>
+
+<style>
+h1 { color: green }
+</style>
