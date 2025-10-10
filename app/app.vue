766c33e9a8c0107add04661158f6881a7e2d371c
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-10-10 11:21:52 +0200

    Learned more about nuxt/vue

--- app/app.vue
+++ app/app.vue
@@ -1,22 +1,52 @@
 <script setup lang="ts">
+
 import { ref } from "vue"
+import Text from "./Text.vue"
+import Header from "./header/Header.vue"
 
 const count = ref({
     test: 0,
-});
+    title: "Hello World!",
+    second: "Hey!",
+})
 
 function increment() {
-    count.value.test++;
+    count.value.test++
 }
+
+function updateTitle(event: FocusEvent) {
+    count.value.title = (event.target as HTMLElement).innerText
+    console.log(count.value)
+}
+
+function updateSecond(event: FocusEvent) {
+    count.value.second = (event.target as HTMLElement).innerText
+    console.log(count.value)
+}
+
+useHead({
+    title: "Recipe Creator",
+})
+
 </script>
 
 <template>
+    <Header />
+    <h1>hello <span>hey</span></h1>
     <button @click="increment">{{ count.test }}</button>
     <div>
-        <h1>Hello World!</h1>
+        <input type="text" v-model="count.title" @input="console.log(count)"></input>
     </div>
+    <Text>
+        Hello World!
+    </Text>
 </template>
 
-<style>
-h1 { color: green }
+<style lang="scss" scoped>
+h1 {
+    color: green;
+    span {
+        color: red;
+    }
+}
 </style>
---
+++ app/header/Header.vue
@@ -0,0 +1,21 @@
+
+<script setup lang="ts">
+import NavItem from './NavItem.vue';
+</script>
+
+<template>
+    <header>
+        <div class="left">
+            {{ $config.public.siteName }}
+        </div>
+        <div class="right">
+            <ul>
+                <NavItem :onClick="() => console.log('hello')">Click me</NavItem>
+                <NavItem>Click me</NavItem>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+</style>
