67e90391c68e29f38d79686f38af0070e9ae4d30
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-10-14 17:46:30 +0200

    Fixed favicon url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
                 // Favicon
                 rel: "icon",
                 type: "image/x-icon",
-                href: "/favicon.png",
+                href: "./favicon.png",
             }]
         }
     }
